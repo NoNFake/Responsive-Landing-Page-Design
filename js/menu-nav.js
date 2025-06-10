f73c20c74c3162762f8dd9ec23fee0f012219929
yurii <uraklimek@gmail.com>
2025-06-10 22:59:26 +0300

    fixes

--- js/menu-nav.js
+++ js/menu-nav.js
@@ -1,18 +1,23 @@
-const headerNav = document.querySelector('header__nav');
-const headerLogin = document.querySelector('header__login');
+const burgerBtn = document.querySelector('.nav-btn');
+const headerNav = document.querySelector('.header__nav');
+const headerLogin = document.querySelector('.header__login');
 
-
-const headerMenuOpenButton = document.querySelector('.menu-nav');
-const headerMenuCloseButton = document.querySelector('.close-nav');
-
-headerMenuOpenButton.addEventListener('click', (e) => {
-    headerNav.classList.add('active');
-    document.body.style.overflow = 'hidden';
+burgerBtn.addEventListener('click', () => {
+    burgerBtn.classList.toggle('active');
+    headerNav.classList.toggle('active');
+    headerLogin.classList.toggle('active');
 });
 
-headerMenuCloseButton.addEventListener('click', () => {
-    headerNav.classList.contains('active')
-        ? headerMenu.classList.remove('active')
-        : null;
-    document.body.style.overflow = 'auto';
-});
+
+// headerMenuOpenButton.addEventListener('click', (e) => {
+//     headerNav.classList.add('active');
+//     headerLogin.classList.add('active')
+//     document.body.style.overflow = 'hidden';
+// });
+
+// headerMenuCloseButton.addEventListener('click', () => {
+//     headerNav.classList.contains('active')
+//         ? headerMenu.classList.remove('active')
+//         : null;
+//     document.body.style.overflow = 'auto';
+// });
